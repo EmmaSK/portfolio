85fdc8af955c19491a7bc92ff62fe72cfc008aca
esk <emseely@gmail.com>
2020-09-10 11:49:00 -0700

    style: init react icons, edit footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { GoMarkGithub } from "react-icons/go";
+import { AiOutlineLinkedin } from "react-icons/ai";
 export default function Footer() {
   return (
     <footer>
-      <h3>Footer</h3>
+      <div>
+        <a href="https://github.com/2006-GHP-NY-Raikou/rat_shopper">
+          <GoMarkGithub color="##e36397" size={32} /> GitHub
+        </a>
+        <p>September 2020</p>
+      </div>
+      <div>
+        {" "}
+        <a href="https://www.linkedin.com/in/emma-seely-katz-b4386a1a3/">
+          <AiOutlineLinkedin size={25} /> Emma Seely-Katz
+        </a>
+      </div>
     </footer>
   );
 }
