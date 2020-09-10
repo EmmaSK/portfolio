681d42f56133a23efd2530bc4f9cc2aa62084fd9
esk <emseely@gmail.com>
2020-09-10 14:30:04 -0700

    heroku

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //routes
-// app.use("/api", require("./api"));
+app.use("/api", require("./api"));
 
 //static mware
 app.use(express.static(path.join(__dirname, "..", "public")));
