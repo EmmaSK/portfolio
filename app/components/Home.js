5533702df08db80aded0e381aa38fb7ee1961984
esk <emseely@gmail.com>
2020-09-16 16:16:14 -0700

    style: fix home page

--- app/components/Home.js
+++ app/components/Home.js
@@ -7,20 +7,26 @@ export default function Home() {
       <div className="title">
         Hi, I'm Emma. Welcome to my corner of the world!
       </div>
-      <p>
-        This site is growing every day – my{" "}
-        <Link to={"/about"}>About Page</Link> is a great place to start!
-      </p>
-      <p>
-        {" "}
-        You can check out my resume{" "}
-        <a
-          href="https://docs.google.com/document/d/1XyDnlVmuPcuJAW7WzRuodM9z4Hc9aKcheYQQj7tv6Go/edit"
-          target="_blank"
-        >
-          here.
-        </a>
-      </p>
+      <div>
+        <p>
+          This site is still growing, but my{" "}
+          <Link to={"/about"}>About Page</Link> is a great place to start.
+        </p>
+        <p>
+          You can check out my resume{" "}
+          <a
+            href="https://docs.google.com/document/d/1XyDnlVmuPcuJAW7WzRuodM9z4Hc9aKcheYQQj7tv6Go/edit"
+            target="_blank"
+          >
+            here.
+          </a>
+        </p>
+        <p>
+          I'd love to hear from you! Reach me at{" "}
+          <a href="mailto:emseely@gmail.com">emseely@gmail.com</a>
+        </p>
+      </div>
+
       <img src="https://static01.nyt.com/images/2019/07/25/fashion/XX-plantlife-sprout1/XX-plantlife-sprout1-jumbo-v3.gif" />
     </div>
   );
